d63f4051c994ce8d4ed209e136ccf40810c6b524
osmokova-tlu <osmokova@tlu.ee>
2024-12-20 02:30:25 +0200

    project
    project

---
+++ My project/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject winMessage;
+
+    void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (enemies.Length == 0)
+        {
+            ShowWinMessage();
+        }
+    }
+
+    void ShowWinMessage()
+    {
+
+        if (winMessage != null)
+        {
+            winMessage.SetActive(true);
+        }
+
+
+        Invoke("RestartGame", 3f);
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
